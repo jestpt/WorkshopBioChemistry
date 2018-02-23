2a85a27d660737059969c970555306192964832f
AJPreto <martinsgomes.jose@gmail.com>
2018-02-23 21:31:56 +0000

    Add files via upload

--- tutorial/bio_tools.py
+++ tutorial/bio_tools.py
@@ -41,21 +41,6 @@ def amino_sequence(input_pdb):
             chain_sequences.append(str("".join(seq)))
     return  chain_sequences
 
-def amino_acid_by_secondary_structure(sequence, secondary_sequence, secondary_structure_tag):
-        
-    ###Fetches the amino acid counts by TM:
-    all_chains = []
-    for chain in sequence:
-        PC_dict = {'G': 0, 'A': 0,'V': 0,'L': 0,'M': 0,'I': 0,'F': 0,
-               'Y': 0,'W': 0,'S': 0,'T': 0,'C': 0,'P': 0,'N': 0,
-               'Q': 0,'K': 0,'R': 0,'H': 0,'D': 0,'E': 0, 'X': 0}
-        for residue, residue_TM in zip(sequence, secondary_sequence):
-            if residue_TM == secondary_structure_tag:
-                PC_dict[residue] = PC_dict[residue] + 1
-            else:
-                continue
-        all_chains.append(PC_dict)
-    return all_chains
 
 def b_factor(input_pdb, input_atom = "CA"):
 
@@ -80,4 +65,5 @@ def b_factor(input_pdb, input_atom = "CA"):
 main = r'C:/Users/A.J. Preto/Desktop/workshop_ANB/biopython'
 os.chdir(main)
 
-write_overall_csv("1h2s.pdb")
+print amino_sequence("1h2s.pdb")
+print b_factor("1h2s.pdb")
